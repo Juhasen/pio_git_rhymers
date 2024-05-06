54ddd0fef1f83203bdffbe28d53e61eaa6f53843
Krystian Juszczyk <juszczyk-krystian@wp.pl>
2024-05-06 21:53:20 +0200

    Zmiana nazwy pola total.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,25 +4,25 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[12];
 
-    private int totalCount = -1;
+    private int total = -1;
 
     protected void countIn(int in) {
         if (!isFull())
-            numbers[++totalCount] = in;
+            numbers[++total] = in;
     }
 
     protected boolean callCheck() {
-        return totalCount == -1;
+        return total == -1;
     }
 
     protected boolean isFull() {
-        return totalCount == 11;
+        return total == 11;
     }
 
     protected int peekaboo() {
         if (callCheck())
             return -1;
-        return numbers[totalCount];
+        return numbers[total];
     }
 
     protected int countOut() {
